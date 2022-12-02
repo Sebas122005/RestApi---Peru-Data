b849111017be264b7f046a76be1240304cca38f6
Seas CC <sebastianbarca03@gmail.com>
2022-12-02 10:10:41 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RestApi---Peru-Data
---
+++ src/main/java/com/pe/proyecto/ProyectowebserviceApplication.java
@@ -0,0 +1,14 @@
+package com.pe.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//un comnetario
+//hola
+@SpringBootApplication
+public class ProyectowebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectowebserviceApplication.class, args);
+	}
+
+}
